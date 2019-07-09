d8d2c9fd72c3ea66f0995c38ea6c3c7513b1af3b
vortex <vortex@void>
2019-07-09 16:08:29 -0400

    forget to fix vim indents

--- gui.py
+++ gui.py
@@ -98,8 +98,8 @@ class NewTransaction:
 
     def send(self):
         if int(self.sstr.get()) == 0:
-		self.text.config(text = "You cannot send nothing")
-	try:
+	        self.text.config(text = "You cannot send nothing")
+	    try:
             if gui.wallet.sendTransaction(self.estr.get(), int(self.sstr.get())) == False:
                 self.text.config(text = "Not enough coins!")
             else:
